aff1d095da5da3ed7f0d2adb9fa003e95a1099e7
Arnav Pandey <www.aakashpandey2004@gmail.com>
2025-08-24 16:59:42 +0530

    Time: 0 ms (100.00%) | Memory: 41.8 MB (42.21%) - LeetSync

--- 1050-construct-binary-search-tree-from-preorder-traversal/construct-binary-search-tree-from-preorder-traversal.java
+++ 1050-construct-binary-search-tree-from-preorder-traversal/construct-binary-search-tree-from-preorder-traversal.java
@@ -14,23 +14,38 @@
  * }
  */
 class Solution {
+    // --O(N^2) Approach
+    // public TreeNode bstFromPreorder(int[] preorder) {
+    //     TreeNode root=null;
+    //     for(int i=0;i<preorder.length;i++){
+    //        root=insert(root,preorder[i]);
+    //     }
+    //     return root;
+    // }
+    // public TreeNode insert(TreeNode root,int val){
+    //     if(root==null){
+    //         TreeNode temp=new TreeNode(val);
+    //         return temp;
+    //     }
+    //     if(root.val>val){
+    //      root.left= insert(root.left,val);
+    //     }
+    //     else
+    //     root.right=insert(root.right,val);
+    //     return root;
+    // }
+    //O(N)  Approach   
+    int idx;
     public TreeNode bstFromPreorder(int[] preorder) {
-        TreeNode root=null;
-        for(int i=0;i<preorder.length;i++){
-           root=insert(root,preorder[i]);
-        }
-        return root;
+        idx = 0;
+        return createBST(preorder,Integer.MIN_VALUE,Integer.MAX_VALUE);
     }
-    public TreeNode insert(TreeNode root,int val){
-        if(root==null){
-            TreeNode temp=new TreeNode(val);
-            return temp;
-        }
-        if(root.val>val){
-         root.left= insert(root.left,val);
-        }
-        else
-        root.right=insert(root.right,val);
-        return root;
+    public TreeNode createBST(int preorder[],int low,int high){
+        if(idx==preorder.length || preorder[idx]<low || preorder[idx]>high)
+        return null;
+        TreeNode temp=new TreeNode(preorder[idx++]);
+        temp.left=createBST(preorder,low,temp.val);
+        temp.right=createBST(preorder,temp.val,high);
+        return temp;
     }
 }
