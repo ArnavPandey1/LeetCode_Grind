3d4511f61b6458a9dd76d290f79f0d09f2100a97
Arnav Pandey <www.aakashpandey2004@gmail.com>
2025-09-08 20:39:05 +0530

    Time: 97 ms (73.32%) | Memory: 61.2 MB (79.43%) - LeetSync

---
+++ 502-ipo/ipo.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int findMaximizedCapital(int k, int w, int[] profits, int[] capital) {
+        int arr[][]=new int[profits.length][2];
+        for(int i=0;i<profits.length;i++){
+            arr[i][0]=profits[i];
+            arr[i][1]=capital[i];
+        }
+        Arrays.sort(arr,(a,b)->a[1]-b[1]);int i=0;
+        PriorityQueue<Integer>pq=new PriorityQueue<>((a,b)->b-a);
+        while(k-->0){
+             while(i<arr.length&& arr[i][1]<=w){
+                pq.add(arr[i][0]);
+                i++;
+             }
+             if(pq.isEmpty()) break;
+             w=w+pq.remove();
+        }
+        return w;
+    }
+}
