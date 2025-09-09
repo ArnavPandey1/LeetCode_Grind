6cfc5d5814ba1f1e3ad41a866ca8599d4ad7c32c
Arnav Pandey <www.aakashpandey2004@gmail.com>
2025-09-10 02:16:07 +0530

    Time: 3 ms (81.09%) | Memory: 42.3 MB (92.36%) - LeetSync

--- 1029-vertical-order-traversal-of-a-binary-tree/vertical-order-traversal-of-a-binary-tree.java
+++ 1029-vertical-order-traversal-of-a-binary-tree/vertical-order-traversal-of-a-binary-tree.java
@@ -17,40 +17,41 @@ class Solution {
     class Pair{
         int row;
         int val;
-        Pair(int row,int val){
-            this.row=row;
+        Pair(int val,int row){
             this.val=val;
+            this.row=row;
         }
     }
     TreeMap<Integer,List<Pair>>map=new TreeMap<>();
     public List<List<Integer>> verticalTraversal(TreeNode root) {
-        List<List<Integer>>ans=new ArrayList<>();
         traverse(root,0,0);
-        for(int key:map.keySet()){
-            List<Pair>l1=map.get(key);
-            //sort by row first then by value;
-            Collections.sort(l1, (a, b) -> {
-                if (a.row == b.row) return a.val - b.val;
-                return a.row - b.row;
+        List<List<Integer>>l1=new ArrayList<>();
+        for(List<Pair>l:map.values()){
+            Collections.sort(l,(a,b)->{
+                if(a.row==b.row){
+                  return a.val-b.val;
+                }
+                else{
+                   return a.row-b.row;
+                }
             });
             List<Integer>l2=new ArrayList<>();
-            for(Pair p:l1){
-                l2.add(p.val);
+            for(int i=0;i<l.size();i++){
+                l2.add(l.get(i).val);
             }
-            ans.add(l2);
+            l1.add(new ArrayList<>(l2));
         }
-        return ans;
+        return l1;
     }
-    public void traverse(TreeNode root,int curr,int row){
+    public void traverse(TreeNode root,int idx,int row){
         if(root==null){
             return ;
         }
-        if(!map.containsKey(curr)){
-            map.put(curr,new ArrayList<>());
+        if(!map.containsKey(idx)){
+            map.put(idx,new ArrayList<Pair>());
         }
-        map.get(curr).add(new Pair(row,root.val));
-        map.put(curr,map.get(curr));
-        traverse(root.left,curr-1,row+1);
-        traverse(root.right,curr+1,row+1);
+        map.get(idx).add(new Pair(root.val,row));
+        traverse(root.left,idx-1,row+1);
+        traverse(root.right,idx+1,row+1);
     }
 }
