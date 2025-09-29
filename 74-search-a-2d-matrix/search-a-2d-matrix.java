15e4fe9230f9b6820bff66cf16e95b9876e5e374
Arnav Pandey <www.aakashpandey2004@gmail.com>
2025-09-29 19:42:34 +0530

    Time: 0 ms (100.00%) | Memory: 42.4 MB (39.05%) - LeetSync

--- 74-search-a-2d-matrix/search-a-2d-matrix.java
+++ 74-search-a-2d-matrix/search-a-2d-matrix.java
@@ -1,28 +1,43 @@
 class Solution {
     public boolean searchMatrix(int[][] matrix, int target) {
-         for(int i=0;i<matrix.length;i++){
-            if(target>=matrix[i][0]&& target<=matrix[i][matrix[0].length-1]){
-             boolean flag=binarysearch(matrix[i],target);
-             if(flag==true)
-             return true;
-            }
-         }    
-         return false;
-    }
-    public boolean binarysearch(int[] arr,int target){
-        int lb=0;int ub=arr.length-1;
-        while(lb<=ub){
-            int mid=lb+(ub-lb)/2;
-            if(arr[mid]==target){
-                return true;
-            }
-            else if(arr[mid]<target){
-                lb=mid+1;
-            }
-            else{
-                ub=mid-1;
-            }
+    //      for(int i=0;i<matrix.length;i++){
+    //         if(target>=matrix[i][0]&& target<=matrix[i][matrix[0].length-1]){
+    //          boolean flag=binarysearch(matrix[i],target);
+    //          if(flag==true)
+    //          return true;
+    //         }
+    //      }    
+    //      return false;
+    // }
+    // public boolean binarysearch(int[] arr,int target){
+    //     int lb=0;int ub=arr.length-1;
+    //     while(lb<=ub){
+    //         int mid=lb+(ub-lb)/2;
+    //         if(arr[mid]==target){
+    //             return true;
+    //         }
+    //         else if(arr[mid]<target){
+    //             lb=mid+1;
+    //         }
+    //         else{
+    //             ub=mid-1;
+    //         }
+    //     }
+    //     return false;
+    // }
+    int lb=0;int ub=matrix.length*matrix[0].length-1;
+    while(lb<=ub){
+        int mid=lb+(ub-lb)/2;
+        if(matrix[mid/matrix[0].length][mid%matrix[0].length]==target){
+            return true;
         }
-        return false;
+        else if(target>matrix[mid/matrix[0].length][mid%matrix[0].length]){
+            lb=mid+1;
+        }
+        else{
+            ub=mid-1;
+        }
     }
+    return false;
+}
 }
