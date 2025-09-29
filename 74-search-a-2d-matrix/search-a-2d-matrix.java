a75267a66ba7501945634b6f315e6e5ddaf9b1de
Arnav Pandey <www.aakashpandey2004@gmail.com>
2025-09-29 18:57:04 +0530

    Time: 0 ms (100.00%) | Memory: 42.2 MB (51.30%) - LeetSync

--- 74-search-a-2d-matrix/search-a-2d-matrix.java
+++ 74-search-a-2d-matrix/search-a-2d-matrix.java
@@ -1,9 +1,11 @@
 class Solution {
     public boolean searchMatrix(int[][] matrix, int target) {
          for(int i=0;i<matrix.length;i++){
+            if(target>=matrix[i][0]&& target<=matrix[i][matrix[0].length-1]){
              boolean flag=binarysearch(matrix[i],target);
              if(flag==true)
              return true;
+            }
          }    
          return false;
     }
