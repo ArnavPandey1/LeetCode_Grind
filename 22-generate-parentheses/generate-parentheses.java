debc52615989b8a508d8710a787053aba366289b
Arnav Pandey <www.aakashpandey2004@gmail.com>
2025-10-03 10:50:58 +0530

    Time: 2 ms (44.35%) | Memory: 43.8 MB (11.07%) - LeetSync

--- 22-generate-parentheses/generate-parentheses.java
+++ 22-generate-parentheses/generate-parentheses.java
@@ -11,7 +11,7 @@ class Solution {
            l1.add(ans);
            return;
         }
-        if(c>o||c>n||o>n){
+        if(c>o||o>n){
             return;
         }
         generate(ans+'(',n,o+1,c);
