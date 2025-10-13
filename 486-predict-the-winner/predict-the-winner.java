3b805dedd5a18ecdff6203ba667a25031d475d5b
Arnav Pandey <www.aakashpandey2004@gmail.com>
2025-10-13 21:07:04 +0530

    Time: 65 ms (30.77%) | Memory: 41.5 MB (12.94%) - LeetSync

--- 486-predict-the-winner/predict-the-winner.java
+++ 486-predict-the-winner/predict-the-winner.java
@@ -1,14 +1,12 @@
 class Solution {
     public boolean predictTheWinner(int[] nums) {
-        return traverse(nums,0,nums.length-1)>=0;
+        return findWinner(nums,0,nums.length-1)>=0;
     }
-    public int traverse(int nums[],int i,int j){
+    public int findWinner(int []nums,int i,int j){
         if(i==j)
         return nums[i];
-        int max=Integer.MIN_VALUE;
-        int first_pick=nums[i]-traverse(nums,i+1,j);
-        int second_pick=nums[j]-traverse(nums,i,j-1);
-        max=Math.max(first_pick,second_pick);
-        return max;
+        int chance1=nums[i]-findWinner(nums,i+1,j);
+        int chance2=nums[j]-findWinner(nums,i,j-1);
+        return Math.max(chance1,chance2);
     }
 }
