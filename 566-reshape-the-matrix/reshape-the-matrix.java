36785f36a22fb768823f100015beb141eb695c21
Arnav Pandey <www.aakashpandey2004@gmail.com>
2026-02-11 12:02:49 +0530

    Time: 1 ms (61.97%) | Memory: 47.4 MB (75.47%) - LeetSync

---
+++ 566-reshape-the-matrix/reshape-the-matrix.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int[][] matrixReshape(int[][] mat, int r, int c) {
+        if(r*c!=mat.length*mat[0].length)
+        return mat;
+        int arr[]=new int[r*c];
+        int ans[][]=new int[r][c];int k=0;
+        for(int i=0;i<mat.length;i++){
+            for(int j=0;j<mat[i].length;j++){
+                arr[k++]=mat[i][j];
+            }
+        }
+         k=0;
+         for(int i=0;i<r;i++){
+            for(int j=0;j<c;j++){
+               ans[i][j]=arr[k++];
+            }
+         }
+         return ans;
+    }
+}
