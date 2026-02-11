8615a755a05f4d1bb5233224807de148d35112cd
Arnav Pandey <www.aakashpandey2004@gmail.com>
2026-02-11 12:38:55 +0530

    Time: 7 ms (17.56%) | Memory: 47.5 MB (7.74%) - LeetSync

---
+++ 1030-smallest-string-starting-from-leaf/smallest-string-starting-from-leaf.java
@@ -0,0 +1,39 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    String min = "~";
+
+    public String smallestFromLeaf(TreeNode root) {
+        helper(root, "");
+        return min;
+    }
+
+    public void helper(TreeNode root, String ans) {
+        if (root == null) {
+            return;
+        }
+        char ch = (char) ('a' + root.val);
+        if (root.left == null && root.right == null) {
+            ans=ch+ans;
+            if (ans.compareTo(min) < 0) {
+                min = "";
+                min = min + ans;
+            }
+        }
+        helper(root.left, ch + ans);
+        helper(root.right, ch + ans);
+    }
+}
