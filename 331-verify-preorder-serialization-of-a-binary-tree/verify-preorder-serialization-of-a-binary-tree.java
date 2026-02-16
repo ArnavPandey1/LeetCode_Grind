48f91e76fdd676fccacd70e69c3865e1c1905c39
Arnav Pandey <www.aakashpandey2004@gmail.com>
2026-02-16 15:33:55 +0530

    Time: 4 ms (95.58%) | Memory: 44.7 MB (91.15%) - LeetSync

--- 331-verify-preorder-serialization-of-a-binary-tree/verify-preorder-serialization-of-a-binary-tree.java
+++ 331-verify-preorder-serialization-of-a-binary-tree/verify-preorder-serialization-of-a-binary-tree.java
@@ -1,11 +1,11 @@
 class Solution {
     public boolean isValidSerialization(String s) {
         String str[]=s.split(",");
-       /* if(str.length>1 && str[0].equals("#"))
-        return false;*/
         int v = 1;
         for(int i=0;i<str.length;i++){
             if(str[i].equals("#")){
+                if(v==0)
+                return false;
                v=v-1;
             }else{
                 if(v==0)
