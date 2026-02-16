761a95b6377af3b4c94d7def6c0e76bdd8229621
Arnav Pandey <www.aakashpandey2004@gmail.com>
2026-02-16 10:51:44 +0530

    Time: 1 ms (60.34%) | Memory: 42.7 MB (34.47%) - LeetSync

--- 190-reverse-bits/reverse-bits.java
+++ 190-reverse-bits/reverse-bits.java
@@ -2,9 +2,11 @@ class Solution {
     public int reverseBits(int n) {
         int ans=0;
         for(int i=1;i<=32;i++){
-            int a=n&1;
+           /* int a=n&1;
             ans=ans<<1;
             ans=ans|a;
+            n=n>>1;*/
+            ans=(ans<<1)|(n&1);
             n=n>>1;
         }
         return ans;
