10c3dfeda725233a4e39e60ad1058e8832407ca4
Arnav Pandey <www.aakashpandey2004@gmail.com>
2026-02-24 20:04:58 +0530

    Time: 2 ms (88.28%) | Memory: 44.1 MB (35.54%) - LeetSync

---
+++ 316-remove-duplicate-letters/remove-duplicate-letters.java
@@ -0,0 +1,28 @@
+class Solution {
+    public String removeDuplicateLetters(String s) {
+        int[] lastIdx=new int[26];
+        for(int i=0;i<s.length();i++){
+            char ch=s.charAt(i);
+            lastIdx[ch-'a']=i;
+        }
+        boolean taken[]=new boolean[26];
+        Stack<Character>st=new Stack<>();
+        for(int i=0;i<s.length();i++){
+            char ch=s.charAt(i);
+            if(taken[ch-'a']){
+                continue;
+            }
+            while(!st.isEmpty() && ch<st.peek() && lastIdx[st.peek()-'a']>i){
+                  char c=st.pop();
+                  taken[c-'a']=false;
+            }
+            st.push(ch);
+            taken[ch-'a']=true;
+        }
+        StringBuilder sb=new StringBuilder();
+        while(!st.isEmpty()){
+            sb.append(st.pop());
+        }
+        return sb.reverse().toString();
+    }
+}
